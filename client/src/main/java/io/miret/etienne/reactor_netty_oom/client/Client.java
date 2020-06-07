9574cacd8337948a8fdeb6a8817ddd6e460e691c
Etienne Miret <etienne.miret@ens-lyon.org>
2020-06-07 14:35:09 +0200

    Create client.

---
+++ client/src/main/java/io/miret/etienne/reactor_netty_oom/client/Client.java
@@ -0,0 +1,36 @@
+package io.miret.etienne.reactor_netty_oom.client;
+
+import io.netty.channel.ChannelOption;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.TcpClient;
+
+public class Client {
+
+  private static final Logger logger = LoggerFactory.getLogger (Client.class);
+
+  private final HttpClient nettyClient;
+
+  public Client (String host, int port) {
+    var tcpClient = TcpClient.create ()
+        .host (host)
+        .port (port);
+    this.nettyClient = HttpClient.from (tcpClient);
+  }
+
+  public Mono<Void> get (int i) {
+    logger.info ("Querying {}.", i);
+    return nettyClient.get ()
+        .uri ("/" + i)
+        .responseContent ()
+        .asByteArray ()
+        .then ()
+        .onErrorResume (Exception.class, e -> {
+          logger.error ("Error on get {}: {}.", i, e.getMessage ());
+          return Mono.empty ();
+        });
+  }
+
+}
---
+++ client/src/main/java/io/miret/etienne/reactor_netty_oom/client/Main.java
@@ -0,0 +1,35 @@
+package io.miret.etienne.reactor_netty_oom.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+
+public class Main {
+
+  private static final Logger logger = LoggerFactory.getLogger (Main.class);
+
+  private static final int BATCH_SIZE = 50;
+
+  public static void main (String[] args) {
+    var host = args[0];
+    var port = Integer.parseInt (args[1]);
+    logger.info ("Connecting to {}:{}.", host, port);
+    var client = new Client (host, port);
+    for (int i = 0; true; i++) {
+      var index = i * BATCH_SIZE;
+      logger.info ("Making {}th batch of {} requests.", i, BATCH_SIZE);
+      Flux.<Integer, Integer>generate (() -> index, (state, sink) -> {
+        if (state < index + 50) {
+          sink.next (state);
+        } else {
+          sink.complete ();
+        }
+        return state + 1;
+      })
+          .flatMap (client::get)
+          .then ()
+          .block ();
+    }
+  }
+
+}
