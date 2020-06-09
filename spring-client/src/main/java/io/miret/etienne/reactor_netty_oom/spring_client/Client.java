837c45f23c7a607097a14ab3c506c5312e12db7f
Etienne Miret <etienne.miret@ens-lyon.org>
2020-06-09 19:36:59 +0200

    Consume the response instead of discarding it.

--- spring-client/src/main/java/io/miret/etienne/reactor_netty_oom/spring_client/Client.java
+++ spring-client/src/main/java/io/miret/etienne/reactor_netty_oom/spring_client/Client.java
@@ -3,7 +3,9 @@ package io.miret.etienne.reactor_netty_oom.spring_client;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
@@ -28,13 +30,14 @@ public class Client {
         .build ();
   }
 
-  public Mono<Integer> get (int i) {
+  public Mono<Long> get (int i) {
     logger.info ("Getting {}.", i);
     return webClient.get ()
         .uri ("/{index}", i)
         .exchange ()
         .delayElement (Duration.ofMillis (200))
-        .flatMap (response -> response.releaseBody ().thenReturn (i));
+        .flatMap (response -> response.bodyToMono (ByteArrayResource.class))
+        .map (ByteArrayResource::contentLength);
   }
 
 }
